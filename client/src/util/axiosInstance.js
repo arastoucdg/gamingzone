f13ca655acd680bc1cf476fe57c6aef5c3d33130
Alireza Saremi <arastoucdg@gmail.com>
2022-02-28 10:58:34 +0100

    fixing server

--- client/src/util/axiosInstance.js
+++ client/src/util/axiosInstance.js
@@ -1,34 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosApiInstance = axios.create();
 
-//defining the settings for our axios instance. 
-axiosApiInstance.defaults.baseURL = 'http://localhost:3001';
-axiosApiInstance.defaults.headers.post['Content-Type'] = 'application/json';
+if (process.env.NODE_ENV == "production") {
+  axiosApiInstance.defaults.baseURL = "https://free-gaming-zone.herokuapp.com";
+} else {
+  axiosApiInstance.defaults.baseURL = "http://localhost:3001";
+}
+//defining the settings for our axios instance.
+
+axiosApiInstance.defaults.headers.post["Content-Type"] = "application/json";
 axiosApiInstance.defaults.withCredentials = true;
 
 //intercepts requests.
-axiosApiInstance.interceptors.request.use(config => {
+axiosApiInstance.interceptors.request.use(
+  (config) => {
     console.log("A request has been made");
     return config;
-}, error => {
+  },
+  (error) => {
     return Promise.reject(error);
-})
+  }
+);
 
 //intercepts responses.
-axiosApiInstance.interceptors.response.use(config => {
+axiosApiInstance.interceptors.response.use(
+  (config) => {
     console.log("A respones has been received");
     return config;
-}, error => {
+  },
+  (error) => {
     console.log("Error response has been received", error.response);
 
-    if(error.response.status === 401){
-        console.log("We hit 401, token must not be valid");
-        window.location = "/logout";
+    if (error.response.status === 401) {
+      console.log("We hit 401, token must not be valid");
+      window.location = "/logout";
     }
 
     return Promise.reject(error);
-})
-
+  }
+);
 
 export default axiosApiInstance;
